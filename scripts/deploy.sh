b9c15e2c7efacc35dde86affd760546bae9c2f95
Thierry Nowak <thinow@gmail.com>
2019-11-04 22:46:35 +0100

    publishes before deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash -ex
 
-SOURCE_CODE_BUCKET_NAME="thierry-nowak-eu-central-1"
-SOURCE_CODE_BUCKET_KEY="aws-lambda-functions/slack-speaks-german/package.zip"
-
 BASE_FOLDER=$(dirname "$0")
 
+# check environment variables
+[[ -z "${AWS_DEFAULT_REGION}" ]] && echo "Error: Missing the environment variable AWS_DEFAULT_REGION" && exit 1
+[[ -z "${BUCKET_NAME}" ]] && echo "Error: Missing the environment variable BUCKET_NAME" && exit 1
+[[ -z "${BUCKET_KEY}" ]] && echo "Error: Missing the environment variable BUCKET_KEY" && exit 1
+
+# Search the source code
+PACKAGE_PATH=$(find "${BASE_FOLDER}/../generated" -name *.zip)
+PACKAGE_FILENAME=$(basename ${PACKAGE_PATH})
+
+[[ -z "${PACKAGE_PATH}" ]] && echo "Error: Package file is missing. Building is required before publishing." && exit 2
+
+# Publishes the source code to S3
+aws s3 cp "${PACKAGE_PATH}" "s3://${BUCKET_NAME}/${BUCKET_KEY}/${PACKAGE_FILENAME}"
+
+# Runs the provisioning
 aws cloudformation deploy \
     --stack-name "slack-speaks-german" \
-    --template-file "${BASE_FOLDER}/../proviosining/lambda-function.template" \
+    --template-file "${BASE_FOLDER}/../provisioning/lambda-function.template" \
     --parameter-overrides \
-        S3BucketName=${SOURCE_CODE_BUCKET_NAME} \
-        S3BucketKey=${SOURCE_CODE_BUCKET_KEY} \
+        S3BucketName=${BUCKET_NAME} \
+        S3BucketKey=${BUCKET_KEY}/${PACKAGE_FILENAME} \
     --capabilities CAPABILITY_IAM
