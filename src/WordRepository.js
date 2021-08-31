d4bd4125b770bbe0315ad71e1b148a8b36867e25
Thierry Nowak <thinow@gmail.com>
2021-08-31 21:57:34 +0200

    extract a single word

--- src/WordRepository.js
+++ src/WordRepository.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const lineReader = require('line-reader')
 const Word = require('./Word')
 
 function assertFileExists(file) {
@@ -12,26 +13,49 @@ function readJsonFile(file) {
 }
 
 class WordRepository {
+
     static loadFromFolder(folder) {
         const dataFile = `${folder}/data.txt`;
         const metadataFile = `${folder}/metadata.json`;
 
         try {
             assertFileExists(dataFile);
-            readJsonFile(metadataFile);
-
-            return new WordRepository()
+            return new WordRepository(dataFile, readJsonFile(metadataFile))
         } catch (cause) {
             throw new Error('folder should be a directory containing the two following files : data.txt, metadata.json')
         }
     }
 
-    getNumberOfWords() {
-        return 0
+    constructor(dataFile, metadata) {
+        this.dataFile = dataFile
+        this.metadata = metadata
     }
 
-    getWord(index) {
-        return new Word('das Wort, -"er', 'word')
+    getNumberOfWords() {
+        return this.metadata.numberOfLines
+    }
+
+    // TODO make all executors of the function async
+    async getWord(index) {
+        const line = await this.readLine(index)
+        return this.transformLineToWord(line)
+    }
+
+    async readLine(index) {
+        const options = {encoding: this.metadata.encoding}
+        return await new Promise(resolve => {
+            // TODO figure out how errors are raised (should it be wrapped with a try/catch?)
+            lineReader.eachLine(this.dataFile, options, function (line, last, readerCallback) {
+                readerCallback(false)
+                // TODO resolve when index has been reached
+                resolve(line)
+            })
+        })
+    }
+
+    transformLineToWord(line) {
+        const [german, , english] = line.split('|');
+        return new Word(german, english)
     }
 }
 
--- test/WordRepository.spec.js
+++ test/WordRepository.spec.js
@@ -3,36 +3,54 @@ const Word = require('../src/Word')
 
 const WRONG_FOLDER_ERROR = 'folder should be a directory containing the two following files : data.txt, metadata.json'
 
-describe('when loading folder used in production', () => {
-    it('should successfully load the repository', () => {
+describe('when loading folder', () => {
+
+    describe('used in production', () => {
+        it('should successfully load the repository', () => {
+            // when
+            const repository = WordRepository.loadFromFolder('./resources/words')
+
+            // then
+            expect(repository).toBeInstanceOf(WordRepository)
+        })
+    })
+
+    describe('which is not a folder', () => {
+        it('should throw an error', () => {
+            expect(() => {
+                WordRepository.loadFromFolder('Not a folder')
+            }).toThrow(WRONG_FOLDER_ERROR)
+        })
+    })
+
+    describe('which does not contain data.txt', () => {
+        it('should throw an error', () => {
+            expect(() => {
+                WordRepository.loadFromFolder('./test/resources/folder-without-data')
+            }).toThrow(WRONG_FOLDER_ERROR)
+        })
+    })
+
+    describe('which does not contain metadata.json', () => {
+        it('should throw an error', () => {
+            expect(() => {
+                WordRepository.loadFromFolder('./test/resources/folder-without-metadata')
+            }).toThrow(WRONG_FOLDER_ERROR)
+        })
+    })
+})
+
+
+describe('with a single word', () => {
+    it('should return the word as the first item', async () => {
         // when
-        const repository = WordRepository.loadFromFolder('./resources/words')
+        const repository = WordRepository.loadFromFolder('./test/resources/folder-single-word');
 
         // then
-        expect(repository).toBeInstanceOf(WordRepository)
-    })
-})
-
-describe('when loading folder which is not a folder', () => {
-    it('should throw an error', () => {
-        expect(() => {
-            WordRepository.loadFromFolder('Not a folder')
-        }).toThrow(WRONG_FOLDER_ERROR)
-    })
-})
-
-describe('when loading folder which does not contain data.txt', () => {
-    it('should throw an error', () => {
-        expect(() => {
-            WordRepository.loadFromFolder('./test/resources/folder-without-data')
-        }).toThrow(WRONG_FOLDER_ERROR)
-    })
-})
-
-describe('when loading folder which does not contain metadata.json', () => {
-    it('should throw an error', () => {
-        expect(() => {
-            WordRepository.loadFromFolder('./test/resources/folder-without-metadata')
-        }).toThrow(WRONG_FOLDER_ERROR)
+        expect(repository.getNumberOfWords()).toEqual(1)
+        expect(await repository.getWord(0)).toEqual(expect.objectContaining(({
+            german: 'einzig',
+            english: 'only, unique',
+        })))
     })
 })
